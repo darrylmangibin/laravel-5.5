c0370aff26fd14c0fdd04ece0c72004b27b49c59
Darryl Mangibin <darryl@goworkwize.com>
2022-03-27 22:00:55 +0800

    Database seeding with faker library

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween,
+        'product_id' => function () {
+            return Product::all()->random();
+        }
     ];
 });
